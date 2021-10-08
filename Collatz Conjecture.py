bc2c09262c4f809b69080eaab08a02e0bf62e212
Xavier Lemmuele C Romano <54741558+AlphaXexia@users.noreply.github.com>
2021-10-08 19:25:55 +0800

    Update Collatz Conjecture.py

--- Collatz Conjecture.py
+++ Collatz Conjecture.py
@@ -1,12 +1,13 @@
 #Collatz conjecture mk. 5
 import matplotlib.pyplot as plt
 
-i = int(input("Give me a Rational Number: ")) #pass the errors
+i = int(input("Give me a Rational Number: "))
 # x = []
 x = [1]
 y = variable_outputs = [i]
 ctr = 1
 if(i>0) :
+    #made for an input when it is equal to 1
     while(i==1):
         if((i%2)==0):
             i//=2
@@ -16,8 +17,10 @@ if(i>0) :
             i=(3*i)+1
             ctr+=1
             x.append(ctr)
+        #This will show the 4-2-1 loop immediately
         variable_outputs.append(i)
-        
+    
+    #made for an input when it is not equal to 1
     while(i != 1):
         if((i%2)==0):
             i//=2
@@ -28,26 +31,25 @@ if(i>0) :
             ctr+=1
             x.append(ctr)
         variable_outputs.append(i)
-        
-    print(x)
-    
-    print("THE COLLATZ CONJECTURE")
-    print(variable_outputs)
     
     # plotting the points
     plt.plot(x, y)
 
     # naming the x axis
-    plt.xlabel('x - axis')
+    plt.xlabel('Number of Variables\nx - axis ')
     # naming the y axis
-    plt.ylabel('y - axis')
+    plt.ylabel('Minimum to Maximum Range of Variables\ny - axis')
 
     # giving a title to my graph
-    plt.title('My first graph!')
+    plt.title('Collatz Conjecture')
 
     # function to show the plot
     plt.show()
+            
+    print("Number of Variables: "+str(x[-1]))
+    
+    print("List of Variables: "+str(variable_outputs))
     
 else:
+    #for any input that is not a Positve Whole Number
     print(str(i)+" is not a Rational Number.")
-    
